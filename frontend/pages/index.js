877f58d01dae31d04477af1902ea4739cdbdb143
dcsyedfaraz <dcsyedfaraz@gmail.com>
2025-07-09 22:35:38 +0500

    fix: Refactor authentication and session handling in NextAuth and update requirements.txt for pandas and scikit-learn

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import { getSession } from 'next-auth/react'
 
-// export async function getServerSideProps(context) {
-//     const session = await getSession(context)
-//     if (!session) {
-//         return { redirect: { destination: '/api/auth/signin', permanent: false } }
-//     }
-//     return { props: { session } }
-// }
+export async function getServerSideProps(context) {
+    const session = await getSession(context)
+    if (!session) {
+        return { redirect: { destination: '/api/auth/signin', permanent: false } }
+    }
+    return { props: { session } }
+}
 
 export default function Home() {
     const [year, setYear] = useState('');
